15fe9f71d7ef88ea25a4e50c73dc51206746c2a9
Deviloka <121934780+Deviloka@users.noreply.github.com>
2024-02-25 21:27:12 +0300

    Bivariate

--- src/FirstApp.java
+++ src/FirstApp.java
@@ -138,6 +138,20 @@ public class FirstApp {
         }
         System.out.println(Arrays.toString(arr));
     }
+//    13. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое), и с помощью цикла(-ов)
+//    заполнить его диагональные элементы единицами (можно только одну из диагоналей, если обе сложно). Определить элементы
+//    одной из диагоналей можно по следующему принципу: индексы таких элементов равны, то есть [0][0], [1][1], [2][2], ..., [n][n];
+    static void bivariate(){
+        int[][] arr = new int[3][3];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0, x = arr[i].length - 1; j < arr[i].length; j++, x--) {
+                if (i == j || i == x) arr[i][j] = 1;
+                else arr[i][j] = 0;
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+    }
 
 
     public static void main (String[] args){
@@ -154,6 +168,7 @@ public class FirstApp {
         emptymassive();
         myFoo();
         multiplication();
+        bivariate();
 
 
     }
