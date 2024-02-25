8761b09f1639d04fd6fe2a8292721756770a24e3
Deviloka <121934780+Deviloka@users.noreply.github.com>
2024-02-25 18:27:41 +0300

    BooleanTrueFalse

--- src/FirstApp.java
+++ src/FirstApp.java
@@ -1,11 +1,14 @@
 import java.util.regex.Matcher;
-
+//1. Создайте метод printThreeWords(), который при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple.
 public class FirstApp {
     static void printThreeWords(){
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
+//    2. Создайте метод checkSumSign(), в теле которого объявите две int переменные a и b, и инициализируйте их любыми
+//    значениями, которыми захотите. Далее метод должен просуммировать эти переменные, и если их сумма больше или равна 0,
+//    то вывести в консоль сообщение “Сумма положительная”, в противном случае - “Сумма отрицательная”;
     static void checkSumSign(){
         int a = 1;
         int b = 2;
@@ -17,6 +20,9 @@ public class FirstApp {
         }
 
     }
+//    3. Создайте метод printColor() в теле которого задайте int переменную value и инициализируйте ее любым значением.
+//    Если value меньше 0 (0 включительно), то в консоль метод должен вывести сообщение “Красный”, если лежит в пределах
+//    от 0 (0 исключительно) до 100 (100 включительно), то “Желтый”, если больше 100 (100 исключительно) - “Зеленый”;
 
     static void printColor(){
         int value = 1;
@@ -29,6 +35,9 @@ public class FirstApp {
             System.out.println("Зеленый");
         }
     }
+//    4. Создайте метод compareNumbers(), в теле которого объявите две int переменные a и b, и инициализируйте их любыми
+//    значениями, которыми захотите. Если a больше или равно b, то необходимо вывести в консоль сообщение “a >= b”,
+//    в противном случае “a < b”;
     static void compareNumbers(){
         int a = 10;
         int b = 2;
@@ -36,6 +45,8 @@ public class FirstApp {
         peremen = (a >= b) ? "a >= b" : "a < b";
         System.out.println (peremen);
     }
+//    5. Напишите метод, принимающий на вход два целых числа и проверяющий, что их сумма лежит в пределах от 10 до 20
+//            (включительно), если да – вернуть true, в противном случае – false.
     static void checkBoolean() {
         int a = 10;
         int b = 10;
@@ -47,6 +58,9 @@ public class FirstApp {
             System.out.println (false);
         }
     }
+//    6. Напишите метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль,
+//    положительное ли число передали или отрицательное. Замечание: ноль считаем положительным числом.
+
     static void comparator(int a) {
         int myNumber =  Integer.signum(a);
         if (myNumber >= 0) {
@@ -56,9 +70,16 @@ public class FirstApp {
         }
 
     }
+//    7. Напишите метод, которому в качестве параметра передается целое число. Метод должен вернуть true, если число отрицательное,
+//    и вернуть false если положительное.
 
-
-
+    static void checkTrueFalse(int a) {
+        if (a < 0) {
+            System.out.print(true);
+        } else {
+            System.out.print(false);
+        }
+    }
 
 
     public static void main (String[] args){
@@ -68,6 +89,7 @@ public class FirstApp {
         compareNumbers();
         checkBoolean();
         comparator(10);
+        checkTrueFalse(5);
 
     }
 }
