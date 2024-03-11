632ced3d8df058dc4e722642e27be4e3c01e6daa
Deviloka <121934780+Deviloka@users.noreply.github.com>
2024-03-11 11:44:34 +0300

    Park

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class  Main {
+    public static void main (String[] args){
+        Park park = new Park();
+        Park.Attractions attraction1 = new Park.Attractions("attraction1", "09:30 - 16:00", "15");
+        System.out.println(attraction1.getName() + " " + attraction1.getTime() + " " + attraction1.getCost());
+    }
+}
