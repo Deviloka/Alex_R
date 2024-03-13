ee0686f94155ff99390382b8025f919a84e0c417
Deviloka <121934780+Deviloka@users.noreply.github.com>
2024-03-13 20:21:57 +0300

    Delete src/Main_Park.java

--- src/Main_Park.java
+++
@@ -1,7 +0,0 @@
-public class  Main_Park {
-    public static void main (String[] args){
-        Park park = new Park();
-        Park.Attractions attraction1 = new Park.Attractions("attraction1", "09:30 - 16:00", "15");
-        System.out.println(attraction1.getName() + " " + attraction1.getTime() + " " + attraction1.getCost());
-    }
-}
