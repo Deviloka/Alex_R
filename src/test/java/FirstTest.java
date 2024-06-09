12c57c0f9d24c20f11a106bb3f32db4d65ef6c70
Deviloka <121934780+Deviloka@users.noreply.github.com>
2024-06-09 19:27:42 +0300

    Created testng test

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,28 +1,25 @@
-import org.junit.jupiter.api.DisplayName;
 import  org.example.Main;
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.testng.Assert.assertEquals;
 
 public class FirstTest {
 
     @Test
-    @DisplayName("Прямой позитивный кейс, вычислить факториал")
     public void testFactorialForPositiveNumber(){
         int result = Main.getFactorial(5);
         assertEquals(120, result);
     }
     @Test
-    @DisplayName("Факториал 0 = 1")
     public void testFactorialForZero() {
         int result = Main.getFactorial(0);
         assertEquals(1, result);
     }
 
     @Test
-    @DisplayName("Нельзя посчитать факториал для отрицательного числа")
-    void exceptionTesting() {
+    void testFactorialForNegativeNumber() {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> Main.getFactorial(-2),
                 "Expected getFactorial() to throw, but it didn't");
         assertEquals("Факториал можно вычислить только для неотрицательных чисел", exception.getMessage());
