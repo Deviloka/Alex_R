d102b16160407966e8a9ad69502ec45c42e0642b
Deviloka <121934780+Deviloka@users.noreply.github.com>
2024-06-09 19:13:52 +0300

    Created junit test

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.DisplayName;
 import  org.example.Main;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
